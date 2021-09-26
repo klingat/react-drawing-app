c3af0747f5ad2d17678b8eab2e6cc4cfb97ac0ba
Kash Lingat <klingat@me.com>
2021-09-25 20:10:02 -0700

    make stroke width picker editable

--- src/components/StrokeWidthPicker.jsx
+++ src/components/StrokeWidthPicker.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { Button } from './Button'
@@ -8,22 +8,55 @@ const StrokePicker = styled.div`
   flex-direction: row;
 `
 
-const StrokeWidth = styled.div`
-  margin: 0 15px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 20px;
-  font-weight: 800;
-`
-
 const PickerButton = styled(Button)`
   width: 40px;
   padding: 0;
   border-radius: 20px;
 `
 
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  border: 2px solid black;
+  background: rgba(255, 255, 255, 0.5);
+  margin: 0 10px;
+
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type='number'] {
+    -moz-appearance: textfield;
+  }
+`
+
+const StrokeInput = styled.input`
+  background: none;
+  border: none;
+  font-size: 20px;
+  font-weight: 800;
+  height: 100%;
+  width: 100%;
+  max-width: 60px;
+  overflow: scroll;
+  padding: 0 10px;
+`
+
 export const StrokeWidthPicker = ({ strokeWidth, onChange }) => {
+  const [strokeWidthInput, setStrokeWidthInput] = useState(strokeWidth)
+
+  useEffect(() => {
+    if (strokeWidth !== strokeWidthInput) {
+      setStrokeWidthInput(strokeWidth)
+    }
+  }, [strokeWidth])
+
   return (
     <StrokePicker>
       <PickerButton
@@ -33,7 +66,18 @@ export const StrokeWidthPicker = ({ strokeWidth, onChange }) => {
       >
         -
       </PickerButton>
-      <StrokeWidth>{strokeWidth}</StrokeWidth>
+      <InputWrapper>
+        <StrokeInput
+          type="number"
+          value={strokeWidthInput}
+          onChange={(event) => setStrokeWidthInput(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === 'Enter' && onChange(event.target.value)
+          }
+          onBlur={(event) => onChange(event.target.value)}
+        />
+      </InputWrapper>
+
       <PickerButton
         onClick={() => {
           onChange(strokeWidth + 1)
